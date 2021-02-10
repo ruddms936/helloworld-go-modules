19fe1a40592412a2e6474c76b7fb97d17a48fae0
Kyungeun <ruddms936@naver.com>
2021-02-11 01:31:12 +0900

    add

--- hello_test.go
+++ hello_test.go
@@ -12,3 +12,9 @@ func TestHello(t *testing.T) {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
+func TestProverb(t *testing.T) {
+	want := "Concurrency is not parallelism."
+	if got := Proverb(); got != want {
+		t.Errorf("Proverb() = %q, want %q", got, want)
+	}
+}
